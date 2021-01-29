ca1be5f55b44267776e8f73b52ed5c7ee90de491
ganesh <ganeshgajula22@gmail.com>
2021-01-29 16:03:07 +0530

    update: creating navbar component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Welcome to my new blog";
-  const likes = 50;
-  const personInfo = ["Ganesh", "20"];
-  // const person = { name: "Ganesh", age: "22" };
-  const link = "https://google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{personInfo}</p>
-        {/* <p>{person}</p> */}
-
-        {/* We can also write any dynamic values inside the curly braces */}
-        <p>{10}</p>
-        <p>{"hello, ninjas"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        {/* Now another thing which we can do inside the JSX is use this dynamic values as attribute values in element text */}
-        {/* For eg. we can have an anchor tag and this has href and normally it has a string value of any website, but in JSX this can be dynamic */}
-
-        <a href={link}>Google Site</a>
+        <h1>App Component</h1>
       </div>
     </div>
   );
