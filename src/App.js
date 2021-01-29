3940acc66bad409e3cc5577f9eaf956e886f3293
ganesh <ganeshgajula22@gmail.com>
2021-01-29 10:19:16 +0530

    update:jsx templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const title = "Welcome to my new blog";
+  const likes = 50;
+  const personInfo = ["Ganesh", "20"];
+  // const person = { name: "Ganesh", age: "22" };
+  const link = "https://google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{personInfo}</p>
+        {/* <p>{person}</p> */}
+
+        {/* We can also write any dynamic values inside the curly braces */}
+        <p>{10}</p>
+        <p>{"hello, ninjas"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        {/* Now another thing which we can do inside the JSX is use this dynamic values as attribute values in element text */}
+        {/* For eg. we can have an anchor tag and this has href and normally it has a string value of any website, but in JSX this can be dynamic */}
+
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
