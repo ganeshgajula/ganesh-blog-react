a0347d7043c6b93842f8f83044ae15396bbd25c9
ganesh <ganeshgajula22@gmail.com>
2021-01-31 21:02:15 +0530

    feat: add click events

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,23 @@
 const Home = () => {
+  const clickHandler = (e) => {
+    console.log("clicked", e.target);
+  };
+
+  const clickAgainHandler = (name, e) => {
+    console.log(name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <button>Click Me</button>.
+      <button onClick={clickHandler}>Click Me</button>
+      <button
+        onClick={(e) => {
+          clickAgainHandler("Ganesh", e);
+        }}
+      >
+        Click me again
+      </button>
     </div>
   );
 };
