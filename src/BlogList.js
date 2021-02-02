e5d5d1057ca16ba4d1a319681c44dc0a4c2565b7
ganesh <ganeshgajula22@gmail.com>
2021-02-02 16:50:45 +0530

    feat: added props

---
+++ src/BlogList.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const BlogList = ({ blogs, title }) => {
+  // const blogs = props.blogs;
+  // const title = props.title;
+  // console.log(props);
+  // console.log(blogs);
+
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => {
+        return (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BlogList;
