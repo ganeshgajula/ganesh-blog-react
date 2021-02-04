8e490a496c5f3cb719f8495acb9799fb271abc18
ganesh <ganeshgajula22@gmail.com>
2021-02-04 10:58:46 +0530

    update:learned useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const BlogList = ({ blogs, title, handleDelete }) => {
   // const blogs = props.blogs;
   // const title = props.title;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -17,6 +17,12 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("useEffect ran");
+    // We can also access the state inside useEffect
+    console.log(blogs);
+  });
 
   return (
     <div className="home">
